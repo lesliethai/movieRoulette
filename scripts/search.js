b5606e6c78a4f1e28053e0f60588ff621e337921
leslie <lesliethai@gmail.com>
2022-10-29 15:18:18 -0700

    Added logo asset & set up search.html and search.js

---
+++ scripts/search.js
@@ -0,0 +1,26 @@
+// Stretch goal: add ability to search movies database/library for movies
+
+// create app object 
+const movie = {}; 
+
+movie.init = () => {
+    console.log(movie);
+}
+
+
+movie.init();
+// console.log(movie.randomizeMovie);
+/*
+// Initialize preset data in the dedicated properties
+// - apiURL : https://imdb-api.com/en/API/
+// - apiKey: k_z8o3ltcf
+
+// write a method that gets the data from the API
+
+// When the API call is successful, display the result by appending the data to the page
+// Clear old selection when you click again
+// If the API call fails, display an error message
+// Create default message to tell user to select something
+
+
+*/
